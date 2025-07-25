a421a2605a7158ab936b6a048014c2167219024e
abdulquawiyy-owolabi <aaowolabi@student.oauife.edu.ng>
2025-07-25 17:37:08 +0100

    modified linetx linesh and linepng

--- tutorials/lines.sh
+++ tutorials/lines.sh
@@ -2,12 +2,14 @@
 
 cat > line.txt <<END
 1 1
-2 2
+10 4
+5 2
 3 3
-4 4
-5 5
+1 1
 END
 
 gmt begin lines png
-    gmt plot line.txt -Jx22c/10c -R0.5/0.5/5.5 -Ba1
+    gmt plot line.txt -JX22c/10c -R0/10/0/5 -Ba1 -W9p,red+solid -Gp19
+        # --PS_LINE_JOIN=bevel
+    # gmt plot line.txt -W1p,white,30_30_5_30
 gmt end show
